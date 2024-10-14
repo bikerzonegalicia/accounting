473006d64fe7f0ca636c4886de8a63ac20a19776
Usuario\Pablo <pablosantosexposito@gmail.com>
2024-10-14 19:54:33 +0200

    master

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,8 @@
 package org.example;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!cxv");
+        System.out.println("Hello World!");
     }
 }
